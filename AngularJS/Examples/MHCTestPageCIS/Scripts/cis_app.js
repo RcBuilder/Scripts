84033178f38341f41986761f4edad1d61ed1b0d9
RcBuilder <RcBuilder@gmail.com>
2018-02-26 14:36:52 +0200

    adding files

---
+++ AngularJS/Examples/MHCTestPageCIS/Scripts/cis_app.js
@@ -0,0 +1,290 @@
+﻿var module = angular.module('my-app', []);
+
+// --- Controllers ------------------
+
+module.controller('homeController', function ($scope, serializer, common, launcher, oauth) {
+
+    // { id, title, startDate, dueDate, callbackUrl, iconURL, scoreType, scorePossible, location }
+    $scope.assignments = [];
+    $scope.jsonResult = '[]';
+    
+    // select inputs
+    $scope.options_toolId = ['Openlearning', 'Simnet', 'Learnsmart'];
+    $scope.options_lmsId = ['Blackboard', 'Openlearning'];
+    $scope.options_scoreType = ['Points', 'Percentage', 'Text', 'Other'];
+
+    $scope.toolId = $scope.options_toolId[0];
+    $scope.lmsId = $scope.options_lmsId[0];
+    $scope.scoreType = $scope.options_scoreType[0];
+
+    // methods
+    $scope.Add = function () {
+        var assignment;
+
+        // fix dates 
+        var startDate = $scope.newAssignment.startDate;
+        if (startDate)
+            startDate = new Date(startDate);
+
+        var dueDate = $scope.newAssignment.dueDate;
+        if (dueDate)
+            dueDate = new Date(dueDate);
+
+        if ($scope.chkGradable) {
+            assignment = new Assignment({
+                id: $scope.newAssignment.id,
+                title: $scope.newAssignment.title,
+                callbackUrl: $scope.newAssignment.callbackUrl,
+                startDate: startDate,
+                dueDate: dueDate,
+                iconURL: $scope.newAssignment.iconURL,
+                location: $scope.newAssignment.location,
+                scoreType: $scope.scoreType,
+                scorePossible: $scope.newAssignment.scorePossible                
+            });
+        }
+        else {
+            assignment = new LinkItem({
+                id: $scope.newAssignment.id,
+                title: $scope.newAssignment.title,
+                callbackUrl: $scope.newAssignment.callbackUrl,
+                startDate: startDate,
+                dueDate: dueDate,
+                iconURL: $scope.newAssignment.iconURL,
+                location: $scope.newAssignment.location,
+            });
+        }
+
+        if (!assignment) return;
+
+        $scope.assignments.push(assignment);
+        $scope.Generate();
+    }
+
+    $scope.Remove = function (index) {
+        $scope.assignments.splice(index, 1);
+        $scope.Generate();
+    }
+
+    $scope.Generate = function () {        
+        var graph = launcher.toGraphArray($scope.assignments);
+        $scope.jsonResult = serializer.serialize(graph, $scope.chkPrettyJson);
+    }
+
+    $scope.Launch = function () {
+        var postData = {};
+
+        var data = new CISData($scope.returnURL, $scope.customerId, $scope.lmsId, $scope.toolId, $scope.recordId, $scope.contextId);
+
+        var contentItems = launcher.toContentItems($scope.assignments);
+        
+        postData['lti_version'] = 'LTI-1p0';
+        postData['lti_message_type'] = 'ContentItemSelection';
+        postData['lti_msg'] = '';
+        postData['lti_errormsg'] = '';
+
+        postData['data'] = serializer.encodedJson(data);
+        postData['content_items'] = serializer.encodedJson(contentItems);
+
+        var host = $scope.host || 'http://mhaairs-aws-qa.tegrity.com'; // 'http://localhost/MHCampus'
+        var launchURL = host.concat('/LTIHandlers/LTIContentItem.ashx');
+
+        // generate an oauth signature
+        var signature = oauth.generate(launchURL, postData, 'FEA488CB1A4844F7B6899DA8770126E3');
+        postData['oauth_signature'] = signature;        
+
+        launcher.launch(launchURL, postData);
+    }
+});
+
+// --- Filters ------------------
+
+module.filter('identity', function () {
+    return function (value) {
+        return '#'.concat(value);
+    };
+});
+
+module.filter('date', function () {
+    return function (value) {
+        var year = value.getFullYear().toString();
+        var month = (value.getMonth() + 1).toString();
+        var day = value.getDate().toString();
+
+        if (month.length == 1) month = '0' + month; // return yyyyMMdd
+        if (day.length == 1) day = '0' + day;
+
+        return year + '-' + month + '-' + day;
+    };
+});
+
+// --- Tools ------------------
+
+// https://github.com/bettiolo/oauth-signature-js
+module.factory('oauth', function () {
+    return {
+        // postData { key: value, key: value ... }
+        generate: function (url, postData, consumerSecret) {
+            // add oauth params as post data
+            postData['oauth_version'] = '1.0';
+            postData['oauth_consumer_key'] = 'simnet';
+            postData['oauth_signature_method'] = 'HMAC-SHA1';
+            postData['oauth_token'] = '1234';
+            postData['oauth_timestamp'] = Math.floor((new Date).getTime() / 1e3);
+            postData['oauth_nonce'] = Math.floor(Math.random() * 1e9).toString();
+
+            var signature = oauthSignature.generate('POST', url, postData, consumerSecret, null, { encodeSignature: false });
+
+            console.log(new oauthSignature.SignatureBaseString('POST', url, postData).generate());
+            console.log(signature);
+
+            return signature;
+        }
+    }
+});
+
+module.factory('serializer', function () {
+    return {
+        serialize: function (obj, pretty) {
+            pretty = pretty || false; // true for a pretty json view instead of a single line
+            return angular.toJson(obj, pretty);
+        },
+        encodedJson: function (obj) {            
+            return encodeURIComponent(JSON.stringify(obj));
+        }
+    }
+});
+
+module.factory('launcher', function () {
+    return {
+        // postData { key: value, key: value ... }
+        launch: function (launchURL, postData) {
+            var form = angular.element('<form method="POST", action="{0}" target="_blank">'.replace('{0}', launchURL));
+
+            for (var p in postData) {                
+                var input = angular.element('<input type="hidden" name="{0}" value="{1}" />'.replace('{0}', p).replace('{1}', postData[p]));
+                form.append(input);
+            }
+
+            angular.element('body').append(form);
+            form.submit();
+            angular.element('body').remove(form);
+        },
+        toContentItems: function (assignments) {
+            return {
+                '@context': 'http://purl.imsglobal.org/ctx/lti/v1/ContentItem',
+                '@graph': this.toGraphArray(assignments)
+            };
+        },
+        toGraphArray: function (assignments) {
+            var graph = [];
+            for (var i = 0; i < assignments.length; i++)
+                graph.push(this.toGraphItem(assignments[i]));
+            return graph;
+        },
+        toGraphItem: function (assignment) {
+            var graphItem = {
+                '@id': assignment.id,
+                '@type': 'LtiLinkItem',
+                mediaType: 'application/vnd.ims.lti.v1.ltilink',
+                url: '',
+                title: assignment.title,
+                text: assignment.title,
+                'mhe:callback_url': assignment.callbackUrl,
+                custom: {},
+                icon: {
+                    URL: ''
+                },
+                placementAdvice: {
+                    presentationDocumentTarget: 'window', // EMBED, FRAME, IFRAME, WINDOW, POPUP, OVERLAY
+                    windowTarget: 'anLTIApp',
+                    'mhe:path': assignment.location
+                },
+                available: {
+                    startDatetime: assignment.startDate,  //'2016-11-07T00:00:00Z'
+                },
+                submission: {
+                    startDatetime: assignment.startDate,  //'2016-11-07T00:00:00Z'
+                    endDatetime: assignment.dueDate
+                }
+            };
+
+            // not exists for NON-Gradable items
+            if (assignment.isGradable()) {
+                graphItem.lineItem = {
+                    '@type': "LineItem",
+                    label: '',
+                    reportingMethod: assignment.scoreType, // 'res:totalScore'
+                    assignedActivity: {
+                        id: '',
+                        activity_id: ''
+                    },
+                    scoreConstraints: {
+                        '@type': 'NumericLimits',
+                        totalMaximum: assignment.scorePossible
+                    }
+                }
+            };
+
+            return graphItem;
+        }
+    }
+});
+
+module.factory('common', function () {
+    return {
+        replace: function (input, oldValue, newValue) {
+            return input.split(oldValue).join(newValue)
+        }
+    }
+});
+
+// --- Entities ------------------
+
+function CISData(returnURL, customerId, lmsId, toolId, recordId, contextId) {
+    this.originalData = '';
+    this.returnURL = returnURL || '';
+    this.customerId = customerId || '';
+    this.lms = lmsId || '';
+    this.toolId = toolId || '';
+    this.originalRecordId = recordId || '';
+    this.contextId = contextId || '';
+}
+
+function LinkItem(params) {
+    var nextWeek = new Date();
+    nextWeek.setDate(nextWeek.getDate() + 7);
+
+    var defaults = { startDate: new Date(), dueDate: nextWeek };
+
+    this.id = params.id || '';
+    this.title = params.title || '';
+    this.callbackUrl = params.callbackUrl || '';
+    this.startDate = params.startDate || defaults.startDate;
+    this.dueDate = params.dueDate || defaults.dueDate;
+    this.location = params.location || '';
+}
+LinkItem.prototype.isGradable = function () {
+    return this.scoreType != undefined;
+}
+
+function Assignment(params) {
+    var defaults = { scoreType: '', scorePossible: 100 };
+
+    LinkItem.call(this, params); // call base constractor (Assignment object is the caller - this)
+    this.scoreType = params.scoreType || defaults.scoreType;
+    this.scorePossible = params.scorePossible || defaults.scorePossible;
+}
+Assignment.prototype = new LinkItem({});
+
+/*
+var l = new LinkItem({});
+var a = new Assignment({});
+    
+alert(l.isGradable()); // false
+alert(a.isGradable()); // true
+
+
+<select ng-options="opt as opt.text for opt in [{ id: 4, text: '444' }, { id: 2, text: '222' }, { id: 1, text: '111' }] track by opt.id" ng-model="ddlTest2" />                        
+<select ng-options="opt as opt for opt in ['aaa','bbb','ccc']" ng-model="ddlTest" />     
+*/
---
+++ AngularJS/Examples/MHCTestPageCIS/TestCIS.aspx
@@ -0,0 +1,181 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" %>
+
+<!DOCTYPE html>
+
+<html ng-app="my-app" lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>CIS GENERATOR</title>
+    <link rel="stylesheet" type="text/css" href="Styles/bootstrap.min.css"  />
+    <link rel="stylesheet" type="text/css" href="Styles/cis_app_style.css" />
+</head>
+<body>
+    <div ng-controller="homeController">
+        <div id="main-wrapper">
+            <div class="row">
+                <div class="col-lg-2">
+                    <h3>items</h3>
+                                        
+                    <div ng-include="'newAssignment.partial'"></div>
+                    <div>
+                        <input type="button" ng-click="Add()" value="Add" class="btn btn-primary" />                        
+                    </div>                    
+                </div>
+                <div class="col-lg-2">
+                    <h3>content items</h3>
+                    <div>
+                        <div class="colorGuide linkItem"></div>
+                        linkItem
+                        &nbsp;&nbsp;
+                        <div class="colorGuide assignment"></div>
+                        assignment
+                    </div>
+                    <ul>
+                        <li ng-repeat="assignment in assignments">
+                            <div ng-include="'assignment.partial'"></div>
+                            <!--<ng-assignment value="assignment"></ng-assignment>-->
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-2">
+                    <h3>launch</h3>                    
+                    <div ng-include="'basicInput.partial'"></div>
+                    <div>
+                        <!-- ng-show="assignments.length > 0" -->
+                        <input type="button" ng-click="Launch()" value="Launch" class="btn btn-primary" />
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <h3>content items (json)</h3>
+                    <div ng-include="'result.partial'"></div>
+                    <div>
+                        <input type="checkbox" ng-model="chkPrettyJson" />
+                        pretty
+                    </div>                    
+                </div>
+            </div>
+        </div>       
+    </div>
+
+    <script type="text/ng-template" id="result.partial">
+        <div class="result-wrapper">
+            <textarea ng-model="$parent.jsonResult" class="form-control" />
+        </div>
+    </script>
+
+    <script type="text/ng-template" id="assignment.partial">
+        <div class="assignment-wrapper">
+            <div ng-class="(assignment.isGradable()) ? 'assignment' : 'linkItem'">                
+                <div>
+                    {{ assignment.id | identity }}
+                </div>
+                <div>
+                    {{ assignment.title }}
+                </div>
+                <div>
+                    start date: {{ assignment.startDate | date }}
+                </div>
+                <div>
+                    due date: {{ assignment.dueDate | date }}
+                </div>
+                <div>
+                    callback: {{ assignment.callbackUrl }}
+                </div>
+                <div>
+                   icon: {{ assignment.iconURL }}
+                </div>
+                <div>
+                   location: {{ assignment.location }}
+                </div>
+
+                <div ng-show="assignment.isGradable()">
+                    <div>
+                        score type: {{ assignment.scoreType }}
+                    </div>
+                    <div>
+                        score possible: {{ assignment.scorePossible }}
+                    </div>
+                </div>
+                
+                <input type="button" ng-click="Remove($index)" value="x" class="btn btn-xs btn-default btn-close" />                
+            </div>
+        </div>
+    </script>
+
+    <script type="text/ng-template" id="basicInput.partial">
+        <div class="new-assignment-wrapper">
+            <div>
+                <input type="text" ng-model="$parent.host" placeholder="host" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.customerId" placeholder="customer Id" class="form-control" />
+            </div>
+            <div>
+                <select ng-model="$parent.toolId" class="form-control" ng-options="opt as opt for opt in options_toolId" />       
+            </div>
+            <div>
+                <select ng-model="$parent.lmsId" class="form-control" ng-options="opt as opt for opt in options_lmsId" />                   
+                </select>                
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.returnURL" placeholder="return URL" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.recordId" placeholder="record Id" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.contextId" placeholder="context Id" class="form-control" />
+            </div>
+        </div>
+    </script>
+
+    <script type="text/ng-template" id="newAssignment.partial">
+        <div class="new-assignment-wrapper">
+            <div>                
+                <input type="text" ng-model="$parent.newAssignment.id" placeholder="id" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.newAssignment.title" placeholder="title" class="form-control" />
+            </div>            
+            <div>
+                <input type="text" ng-model="$parent.newAssignment.startDate" placeholder="start Date yyyy-MM-dd" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.newAssignment.dueDate" placeholder="due Date yyyy-MM-dd" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.newAssignment.callbackUrl" placeholder="callback URL" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.newAssignment.iconURL" placeholder="icon URL" class="form-control" />
+            </div>
+            <div>
+                <input type="text" ng-model="$parent.newAssignment.location" placeholder="location" class="form-control" />
+            </div>
+
+            <div>                
+                <input type="checkbox" ng-model="$parent.chkGradable" />
+                gradable
+            </div>
+            <div ng-show="chkGradable">
+                <div>
+                    <select ng-model="$parent.scoreType" class="form-control" ng-options="opt as opt for opt in options_scoreType" />
+                </div>
+                <div>
+                    <input type="text" ng-model="$parent.newAssignment.scorePossible" placeholder="score Possible" class="form-control" />
+                </div>
+            </div>
+        </div>
+    </script>
+
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    
+    <script src="Scripts/hmac-sha1.js"></script>    
+    <script src="Scripts/enc-base64-min.js"></script>
+    <script src="Scripts/url.min.js"></script>
+    <script src="Scripts/oauth-signature.js"></script>
+
+    <script type="text/javascript" src="https://code.angularjs.org/1.4.9/angular.min.js"></script>
+    <script type="text/javascript" src="Scripts/cis_app.js"></script>
+</body>
+</html>
