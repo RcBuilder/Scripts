4a7543a9cbd27d2b25c8bb24d3fdc96f372d5f0b
RcBuilder <RcBuilder@gmail.com>
2020-06-16 00:15:46 +0300

    adding files

--- NodeJS/Sources/Base Server/server.js
+++ NodeJS/Sources/Base Server/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express');  // npm install --save express
 const app = express();
 const cors = require('cors');  // npm install --save cors
 const routes = require('./routes');
@@ -6,6 +6,8 @@ const config = require('./config.json');
 
 // set views folder
 app.set('views', __dirname + '/views');
+
+// set cors
 app.use(cors());
 
 // static content middleware
@@ -14,6 +16,19 @@ app.use(express.static(__dirname + '/static'));
 // add jsx engine
 app.engine('jsx', require('express-react-views').createEngine());  // npm install express-react-views react react-dom
 app.set('view engine', 'jsx');
+
+// add body parser middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());  // npm install --save body-parser 
+
+// custom middlewares
+app.use(
+    (request, response, next) => {
+        console.log('app-middleware-A');
+        next();
+    }
+);
+// more custom middlewares here ...
 
 // register routes
 app.use('/', routes);
